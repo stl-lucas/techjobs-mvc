fed4bfe1a62244ec3c58c52a9530cbd8dc749a8b
Tim Lucas <tllucas1s@gmail.com>
2017-12-18 15:58:34 -0600

    Completed assignment including search handler, displaying search results, and display list of all jobs using fragments.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
 /**
  * Created by LaunchCode
  */
@@ -23,5 +24,23 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String listResultsByColumnAndValue(Model model,
+                                              @RequestParam String searchType, @RequestParam String searchTerm) {
+        if (searchType.equals("all")){
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("title", "Jobs with " + searchTerm);
+            model.addAttribute("jobs", jobs);
 
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("title", "Jobs with " + searchTerm);
+            model.addAttribute("jobs", jobs);
+
+            return "search";
+        }
+    }
 }
